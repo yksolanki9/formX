effe998815332ccade4f7f7dd35d0a7df85d681c
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-23 20:48:38 +0530

    Add material-ui to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../utils/theme";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
