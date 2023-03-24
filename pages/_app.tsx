0ad70a26fc47d974afbf577b65dabf2e11a75ade
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-24 20:20:38 +0530

    Use Questrial font for the project and import it from nextjs fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,20 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../utils/theme";
 
+import { Questrial } from "@next/font/google";
+const questrial = Questrial({
+  weight: "400",
+  style: "normal",
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <div className={questrial.className}>
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 }
