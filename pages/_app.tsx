3043fd10a6a57aed3a26eb84ad15b7ce11b5e1e1
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-29 19:22:26 +0530

    Fix tailwind issues in prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,10 @@ import type { AppProps } from 'next/app'
 
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../utils/theme";
-
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 import { Questrial } from "next/font/google";
+
 const questrial = Questrial({
   weight: "400",
   style: "normal",
@@ -12,12 +14,19 @@ const questrial = Questrial({
   display: "swap",
 });
 
+const cache = createCache({
+  key: "css",
+  prepend: true,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <div className={questrial.className}>
-        <Component {...pageProps} />
-      </div>
-    </ThemeProvider>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <div className={questrial.className}>
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,4 +21,5 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
+  important: "#__next",
 };
