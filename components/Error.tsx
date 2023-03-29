602e94aa20426e000b964d48f752f43d25beb22b
yksolanki9 <yashsolanki1709@gmail.com>
2023-03-30 00:39:26 +0530

    Minor

--- components/Error.tsx
+++ components/Error.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function Error({ error }: Props) {
   return (
-    <div className="bg-light-pink rounded h-8 mt-4 flex items-center py-1 px-2 sm:pr-3 font-sans w-full sm:w-fit">
+    <div className="bg-light-pink rounded mt-4 flex items-center py-1 px-2 sm:pr-3 font-sans w-full sm:w-fit">
       <WarningRoundedIcon color="error" sx={{ fontSize: 18 }} />
       <div className="text-pink-900 text-sm pl-1">{error}</div>
     </div>
